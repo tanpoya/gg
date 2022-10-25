b54970ccbfbdfee81004ce0bb59974e3be71d083
tanpoya <tanpoya02@gmail.com>
2022-10-25 13:51:55 +0900

    Update main.js

--- 04.JS학습/JS응용구현/Ossomae_PJ (2)/js/main.js
+++ 04.JS학습/JS응용구현/Ossomae_PJ (2)/js/main.js
@@ -71,10 +71,18 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // 인터발용변수
     let autoI;
+    // 타임아웃용 변수
+    let autoT;
 
     // 자동넘기기
     // 인터발함수를 지우려면 변수에 넣고
     // clearInterval(변슈) 해야함
+
+    /* 
+        함수명: slideAuto
+        기능: 자동슬라이드
+    
+    */
     function slideAuto() {
         autoI = setInterval(() => {
             goSlide(1);
@@ -83,10 +91,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // 인터발함수 최초호출
     slideAuto();
-
-    // 타임아웃용 변수
-    let autoT;
-
+    
+    
+    /* 
+        함수명: clearAuto
+        기능: 인터발지우기, 타임아웃세팅
+    */
     // 인터발 지우기 함수
     function clearAuto() {
         console.log("지우기");
@@ -96,6 +106,6 @@ window.addEventListener("DOMContentLoaded", () => {
         clearTimeout(autoT);
         // 3. 일정시간 후 다시 인터발 호출
         autoT = setTimeout(slideAuto, 5000);
-
+        
     };
 }); ///////////// 로드구역 ///////////////////
