21c37503bac46dfb573e3cb14b107e3613601a79
tanpoya <tanpoya02@gmail.com>
2022-10-31 14:37:37 +0900

    1

---
+++ 02.HTML 학습/08.VOGUE_PJ/002.site/js/common.js
@@ -0,0 +1,38 @@
+// 보그 PJ 공통JS - common.js
+
+// 제이쿼리 구역 길게쓰기도 있음
+// $(document).ready(function(){})
+
+$(()=>{ ///////////// jQB //////////////////////
+
+    console.log("로딩완료");
+
+    ////// 스크롤 이벤트 구역
+    // 이벤트명: scroll
+    // 이벤트 대상: window
+    const topA = $("#top");
+
+    // 스크롤위치변수
+    let scTop;
+
+    $(window).scroll(function(){
+        // 스크롤 위치값
+        scTop = $(this).scrollTop();
+        //scrollTop() 메서드 - 세로스크롤 위치값을 리턴하는 메서드
+        console.log(scTop);
+
+        // 1. 슬림메뉴 클래스on 적용
+        // 기준위치는 스크롤위치 100px 이상
+
+        if(scTop >= 100) { // 100px 이상
+            topA.addClass("on");
+        } else { // 100px 미만
+            topA.removeClass("on");
+            // removeClass(클래스명) - 클래스지우기
+        } //////// if ///////////
+
+    }); /////////// scroll ///////////
+
+
+
+}); ///////////// jQB //////////////////////
