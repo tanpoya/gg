7a8c469a3111afac47538cad6d3dd22bc568232e
tanpoya <tanpoya02@gmail.com>
2022-11-25 10:46:20 +0900

    1

--- 02.HTML 학습/08.VOGUE_PJ/002.site copy/js/cm.js
+++ 02.HTML 학습/08.VOGUE_PJ/002.site copy/js/cm.js
@@ -1,40 +1,12 @@
 // 보그PJ 공통JS - common.js
 
-// 윈도우 가로 크기가 모바일 사이즈면 코드를 변경한다!
-// 모바일일때 적용하고 싶거나 싶지않으면 mobsts값을 활용한다!
-// 모바일일때 1, 모바일이 아닐때 0 (가로크기 500기준)
-let mobsts = 0;
-if ($(window).width() <= 500) mobsts = 1;
-console.log("모바일적용여부:", mobsts);
-
-// 이건 순전히 개발자들 브라우저 크기변경 테스트때문에 구현함
-$(window).resize(() => {
-    // 브라우저 화면크기변경시 계속발생!
-    if ($(window).width() <= 500) mobsts = 1;
-    else mobsts = 0;
-    console.log("모바일적용여부(resize):", mobsts);
-    
-    // 탑영역 스타일 날리기!
-    $("#top").attr("style","");
-}); /////// resize //////////////
-
-// [현재 페이지명을 알아내어 제어에 활용한다!]
-// 페이지명 변수
-let pname = location.pathname;
-// location.pathname 페이지명이 포함된 전체경로
-// split(자를문자열) -> 배열에 담긴다!
-pname = pname.split("/"); //배열에 담는다
-pname = pname[pname.length - 1]; // 마지막 배열
-pname = pname.split(".")[0]; // 페이지이름만 가져옴
-// console.log("페이지이름:",pname);
-
 // 제이쿼리 구역 길게쓰기도 있음~!
 // $(document).ready(function(){})
 
 $(() => {
     //////////// jQB ///////////////////
 
-    // console.log("로딩완료!");
+    console.log("로딩완료!");
 
     /// 부드러운 스크롤 호출!(제이쿼리 아님!)
     startSS();
@@ -81,18 +53,11 @@ $(() => {
     }); /////////// each 메서드 ///////////
 
     // 위치배열값 확인하기!
-    // console.log("위치배열값:", scpos);
+    console.log("위치배열값:", scpos);
 
     ///////////////////////////////////////
     //////// 스크롤 이벤트 함수 /////////////
     $(window).scroll(() => {
-        // 슬림메뉴와 상단이동버튼 보이기 작동안할 페이지셋팅
-        if (pname === "login" || 
-        pname === "member" || 
-        pname === "gallery") {
-            return; // 여기서 나감!
-        } ////////// if /////////////
-
         // 스크롤 위치값(this는 window)
         scTop = $(this).scrollTop();
         // scrollTop() 메서드
@@ -101,8 +66,7 @@ $(() => {
 
         // 1. 슬림메뉴 클래스on적용
         // 기준위치는 스크롤위치 100px이상
-        // mobsts === 0 모바일이 아닐때만 들어옴!
-        if (scTop >= 100 && mobsts === 0) {
+        if (scTop >= 100) {
             // 100px이상
             topA.addClass("on");
             // addClass(클래스명) - 클래스넣기
@@ -156,7 +120,7 @@ $(() => {
         // 3. 등장액션 적용하기 ///////
         // 스크롤 등장액션 검사함수 호출!
         // 등장요소 개수만큼 자동으로 돌아주면 호출!
-        scAct.each((idx) => scAction(idx));
+        scAct.each(idx=>scAction(idx));
     }); //////// scroll /////////////////
 
     /**************************************** 
@@ -197,63 +161,6 @@ $(() => {
         // 위치이동후 부드러운 스크롤위치로 다시이동됨!
         // pos 전역변수에 값넣기
         pos = 0; // 최상단 위치인 0을 넣는다!
+
     }); /////// click ///////////
-
-    /************************************** 
-        모바일 버튼 클릭시 검색 / GNB 보이기
-    **************************************/
-    // 1. 햄버거버튼
-    $('.hbtn').click(() => {
-        // GNB 박스 슬라이딩 보임/숨김
-        // slideToggle() -> slideUp/slideDown
-        // 대상: 모바일 GNB
-        $('#mobx').slideToggle(400, resetH);
-        // 애니후 높이값 재설정 호출!
-    }); ////////// click /////////
-
-    // 2. 검색버튼
-    $('.sbtn').click(() => {
-        // GNB 박스 슬라이딩 보임/숨김
-        // slideToggle() -> slideUp/slideDown
-        // 대상: 모바일 GNB
-        $('.mos').slideToggle(200, resetH);
-        // 애니후 높이값 재설정 호출!
-    }); ////////// click /////////
-
-    /* 모바일 GNB 높이값 재설정 함수 */
-    const resetH = () => {
-        // #mobx의 높이값을 동적으로 생성함
-        console.log(
-            '.top의 높이값:',
-            $('.top').innerHeight(),
-            '\n.mos의 높이값:',
-            $('.mos').css('display')
-        );
-
-        // 검색박스 높이는 block일때 넣어줌!
-        let temp = $('.mos').css('display') === 'none' ?
-            0 : $('.mos').innerHeight();
-
-        // 변경할 높이
-        let Hval =
-            $('.top').innerHeight() + temp;
-        // innerHeight()는 패딩포함높이
-        // height()는 순수높이(컨텐츠만)
-        // 017문서참조
-
-        console.log('.top높이:',
-            $('.top').innerHeight());
-
-        console.log('.mos높이:',
-            $('.mos').css('display') === 'none' ?
-            0 : $('.mos').innerHeight());
-
-        console.log('보정높이:', Hval);
-
-        // 동적으로 변경하기
-        $("#mobx").css({
-            height: `calc(100vh - ${Hval}px)`
-        })
-    }; /////// resetH 함수 ///////////
-
 }); //////////////// jQB ///////////////////
