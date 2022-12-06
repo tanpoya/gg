3d5799801101c3d468768c18ab59539468214a44
tanpoya <tanpoya02@gmail.com>
2022-12-06 14:33:50 +0900

    1

--- 06.React학습/react-app/my-react-app/src/DC/Layout.js
+++ 06.React학습/react-app/my-react-app/src/DC/Layout.js
@@ -1,23 +1,29 @@
 import {Outlet, Link} from "react-router-dom";
 import Logo from "./Logo";
 import "./Layout.css";
+import "https://kit.fontawesome.com/a076d05399.js";
 /* 
     [ 리엑트 스타일링 기법 3가지 ]
-    1. 일반 css파일을 컴포넌트 js파일에 import하여 사용
+    1. 일반 CSS파일을 컴포넌트 JS파일에 import하여 사용
+    예) import "CSS경로";
+
     2. 객체를 만들어서 적용하는 방법
     예)
-    const mystyle = {color:"red",fontSize:"30px"}
-    <h1 style={mystyle}></h1>
+    const mystyle = {color:"red",fontSize:"30px"};
+    <h1 style={mystyle}>하하하</h1>
+
     3. 직접 중괄호 표현식에 중괄호를 하나 더 하여 표현
+    -> 중괄호 하나는 표현식, 안에 중괄호는 객체(CSS용)
     예)
-    <h1 style={{color:"red"}}></h1>
+    <h1 style={{color:"red"}}>하하하</h1>
 */
 
 const Layout = () => {
     return(
         <>
-        {/* 1, 상단영역 */}
+        {/* 1.상단영역 */}
         <header className="top">
+        {/* 네비게이션 파트 */}
             <nav className="gnb">
                 <ul>
                     <li>
@@ -44,21 +50,29 @@ const Layout = () => {
                     <li>
                         <Link to="/nw">NEWS</Link>
                     </li>
+                    <li>
+                        <Link to="/mem">
+                            <i className='fas fa-users' style={{fontSize:"36px",color:"lime"}}></i>
+                        </Link>
+                    </li>
                 </ul>
             </nav>
-            </header>
-            {/* 2. 메인영역 */}
-            <main className="cont">
-            {/* 출력파트 : 각 페이지가 표시됨 */}  
+        </header>
+        {/* 2.메인영역 */}
+        <main className="cont">         
+            {/* 출력파트 : 각 페이지가 표시됨 */}
             <Outlet />
-
-            </main>
-            <footer className="info">
-            All Site Content © &amp; TM DC, unless otherwise noted here. All rights reserved. <br />
-            </footer>
+        </main>
+        {/* 3.하단영역 */}
+        <footer className="info">
+            All Site Content © &amp; TM DC, unless otherwise noted here.
+            <br /> 
+            All rights reserved. 
+        </footer>
+           
         </>
     );
 };
 
-// 내보내기 필수
+// 내보내기 필수!
 export default Layout;
